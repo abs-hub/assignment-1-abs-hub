36e4dfbbeb22e5e4107711ce18c68982895a22d4
Abhishek Siriya <abs337@g.harvard.edu>
2020-02-04 00:14:06 -0800

    code comments

--- stringserver.js
+++ stringserver.js
@@ -3,7 +3,9 @@ var http = require('http');
 // create server function with request and response
 var server = http.createServer(function(req, res) {
     res.writeHead(200); // for success 
+    // write a custom message
     res.write("Hello World! Welcome to abs-hub!");
+    // response end
     res.end();
 });
 // add a listener for 8080 port, we need to open the port 8080
